6f60dd3180b8b59a058e7c151e31764aa0cf6dd9
shurikVish <shurik.vish@gmail.com>
2020-08-06 06:14:43 +0300

    1.21 1.22

---
+++ chapter1/1.22.rkt
@@ -0,0 +1,26 @@
+#lang racket
+
+(require "1.21.rkt")
+
+(define (timed-prime-test n)
+  (start-prime-test n (current-inexact-milliseconds)))
+
+(define (start-prime-test n start-time)
+  (if (prime? n)
+      (begin (display n) (report-prime (- (current-inexact-milliseconds) start-time)))
+      #f))
+
+(define (report-prime elapsed-time)
+  (display " --- ")
+  (display elapsed-time)
+  (newline))
+
+(define (search-for-primes low up n)
+  (define (search low up n)
+    (cond [(= n 0) #f]
+          [(and (< low up) (not (timed-prime-test low))) (search-for-primes (+ low 2) up n)]
+          [(< low up) (search-for-primes (+ low 2) up (- n 1))]
+          [else #f]))
+  (if (even? low) (search (+ low 1) up n) (search low up n)))
+
+
