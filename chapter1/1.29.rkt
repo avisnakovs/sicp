f408d31126e77981083086b88f840229d186c985
shurikVish <shurik.vish@gmail.com>
2020-08-08 07:37:49 +0300

    1.30

--- chapter1/1.29.rkt
+++ chapter1/1.29.rkt
@@ -11,9 +11,11 @@
 (define (inc x) (+ 1 x))
 
 (define (sum term a next b)
-  (if (> a b)
-      0
-      (+ (term a) (sum term (next a) next b))))
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (+ (term a) result))))
+  (iter a 0))
 
 (define (coefficient k)
   (cond ((= k 0) 1)
